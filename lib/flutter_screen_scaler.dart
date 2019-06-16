29f0ad0d6bd6032c8f7c3e9989a9165f83cde657
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-06-17 00:46:12 +0530

    Added code for dynamic height, width & text size.

--- lib/flutter_screen_scaler.dart
+++ lib/flutter_screen_scaler.dart
@@ -1,7 +1,27 @@
 library flutter_screen_scaler;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+
+/// A package to resize your widgets according to the screen size with the use of percentages.
+class ScreenScaler {
+  static double _screenWidth = 1080.0;
+  static double _screenHeight = 1920.0;
+
+  void init(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    _screenWidth = mediaQuery.size.width;
+    _screenHeight = mediaQuery.size.height;
+  }
+
+  /// Returns the width equivalent of the [percentage] provided.
+  double getWidth(double percentage) => _screenWidth * (percentage / 100);
+
+  /// Returns the height equivalent of the [percentage] provided.
+  double getHeight(double percentage) => _screenHeight * (percentage / 100);
+
+  /// Returns the text size for the [percentage] provided.
+  double getTextSize(double percentage) =>
+      percentage / 100 * (getHeight(percentage) + getWidth(percentage));
 }
--- test/flutter_screen_scaler_test.dart
+++ test/flutter_screen_scaler_test.dart
@@ -3,11 +3,49 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_screen_scaler/flutter_screen_scaler.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('returns the height equivalent of the percentage provided', () {
+    final screenScaler = ScreenScaler();
+
+    /// first test case result
+    double first = 10 / 100 * 1920;
+
+    /// second test case result
+    double second = 50 / 100 * 1920;
+
+    expect(screenScaler.getHeight(10), first);
+    expect(screenScaler.getHeight(50), second);
+
+    expect(() => screenScaler.getHeight(null), throwsNoSuchMethodError);
+  });
+
+  test('returns the width equivalent for the percentage provided', () {
+    final screenScaler = ScreenScaler();
+
+    /// first test case result
+    double first = 5 / 100 * 1080;
+
+    /// second test case result
+    double second = 45 / 100 * 1080;
+
+    expect(screenScaler.getWidth(5), first);
+    expect(screenScaler.getWidth(45), second);
+
+    expect(() => screenScaler.getWidth(null), throwsNoSuchMethodError);
+  });
+  test('returns the text size for the percentage provided', () {
+    final screenScaler = ScreenScaler();
+
+    /// first test case result
+    double first =
+        15 / 100 * (screenScaler.getHeight(15) + screenScaler.getWidth(15));
+
+    /// second test case result
+    double second =
+        25 / 100 * (screenScaler.getHeight(25) + screenScaler.getWidth(25));
+
+    expect(screenScaler.getTextSize(15), first);
+    expect(screenScaler.getTextSize(25), second);
+
+    expect(() => screenScaler.getTextSize(null), throwsNoSuchMethodError);
   });
 }
