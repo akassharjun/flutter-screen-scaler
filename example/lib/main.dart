48d44c107f9c5c67c90dae44b9d05d16304846db
akassharjun <akassharjun@gmail.com>
2025-06-05 17:31:24 +1000

    updated code to adopt latest changes in flutter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class ExamplePage extends StatefulWidget {
-  ExamplePage({Key? key}) : super(key: key);
+  const ExamplePage({Key? key}) : super(key: key);
 
   @override
   _ExamplePageState createState() => _ExamplePageState();
@@ -31,7 +31,7 @@ class ExamplePage extends StatefulWidget {
 class _ExamplePageState extends State<ExamplePage> {
   @override
   Widget build(BuildContext context) {
-    ScreenScaler scaler = ScreenScaler()..init(context);
+    ScreenScaler.init(context);
 
     return Scaffold(
       body: Container(
@@ -44,75 +44,140 @@ class _ExamplePageState extends State<ExamplePage> {
               alignment: Alignment.center,
               color: Colors.white,
               // take 50% of the height
-              height: scaler.getHeight(50),
+              height: 50.h,
               // take 100% of the width
-              width: scaler.getWidth(100),
+              width: 100.w,
               child: Text(
                 "Height:50%\nWidth:100%\nFont Size:15%",
                 style: TextStyle(
-                  fontSize: scaler.getTextSize(15),
+                  fontSize: 15.sp,
                 ),
               ),
             ),
             Row(
-              children: <Widget> [
+              children: <Widget>[
                 Container(
                   alignment: Alignment.center,
                   color: Colors.green,
-                  // take 25% of the height
-                  height: scaler.getHeight(50),
+                  // take 50% of the height
+                  height: 50.h,
                   // take 50% of the width
-                  width: scaler.getWidth(50),
+                  width: 50.w,
                   child: Text(
                     "Height:50%\nWidth:50%\nFont Size:14%",
                     style: TextStyle(
-                    fontSize: scaler.getTextSize(14),
+                      fontSize: 14.sp,
                     ),
                   ),
-                ), 
+                ),
                 Container(
                   alignment: Alignment.center,
                   color: Colors.blue,
                   // take 25% of the height
-                  height: scaler.getHeight(25),
+                  height: 25.h,
                   // take 25% of the width
-                  width: scaler.getWidth(25),
+                  width: 25.w,
                   child: Text(
                     "Height:25%\nWidth:25%\nFont Size:10%",
                     style: TextStyle(
-                    fontSize: scaler.getTextSize(10),
+                      fontSize: 10.sp,
                     ),
                   ),
-                ), 
-                Container(
-                  alignment: Alignment.center,
-                  color: Colors.red,
-                  // take 12.5% of the height
-                  height: scaler.getHeight(12.5),
-                  // take 12.5% of the width
-                  width: scaler.getWidth(12.5),
-                  child: Text(
-                    "Height:12.5%\nWidth:12.5%\nFont Size:8%",
-                    style: TextStyle(
-                    fontSize: scaler.getTextSize(8),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      color: Colors.purple,
+                      // take 12.5% of the height
+                      height: 12.5.h,
+                      // take 12.5% of the width
+                      width: 12.5.w,
+                      child: Text(
+                        "Height:12.5%\nWidth:12.5%\nFont Size:8%",
+                        style: TextStyle(
+                          fontSize: 8.sp,
+                        ),
+                      ),
                     ),
-                  ),
-                ), 
-                Container(
-                  alignment: Alignment.center,
-                  color: Colors.orange,
-                  // take 50% of the height
-                  height: scaler.getHeight(50),
-                  // take 12.5% of the width
-                  width: scaler.getWidth(12.5),
-                  child: Text(
-                    "Height:50%\nWidth:12.5%\nFont Size:8%",
-                    style: TextStyle(
-                    fontSize: scaler.getTextSize(8),
+                    Container(
+                      alignment: Alignment.center,
+                      color: Colors.red,
+                      // take 12.5% of the height
+                      height: 12.5.h,
+                      // take 12.5% of the width
+                      width: 12.5.w,
+                      child: Text(
+                        "Height:12.5%\nWidth:12.5%\nFont Size:8%",
+                        style: TextStyle(
+                          fontSize: 8.sp,
+                        ),
+                      ),
                     ),
-                  ),
-                ), 
-            ])
+                    Container(
+                      alignment: Alignment.center,
+                      color: Colors.green,
+                      // take 12.5% of the height
+                      height: 12.5.h,
+                      // take 12.5% of the width
+                      width: 12.5.w,
+                      child: Text(
+                        "Height:12.5%\nWidth:12.5%\nFont Size:8%",
+                        style: TextStyle(
+                          fontSize: 8.sp,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      color: Colors.blue,
+                      // take 12.5% of the height
+                      height: 12.5.h,
+                      // take 12.5% of the width
+                      width: 12.5.w,
+                      child: Text(
+                        "Height:12.5%\nWidth:12.5%\nFont Size:8%",
+                        style: TextStyle(
+                          fontSize: 8.sp,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      color: Colors.orange,
+                      // take 25% of the height (corrected from 50% in comment)
+                      height: 25.h,
+                      // take 12.5% of the width
+                      width: 12.5.w,
+                      child: Text(
+                        "Height:25%\nWidth:12.5%\nFont Size:8%",
+                        style: TextStyle(
+                          fontSize: 8.sp,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      color: Colors.pink,
+                      // take 25% of the height (corrected from 50% in comment)
+                      height: 25.h,
+                      // take 12.5% of the width
+                      width: 12.5.w,
+                      child: Text(
+                        "Height:25%\nWidth:12.5%\nFont Size:8%",
+                        style: TextStyle(
+                          fontSize: 8.sp,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ),
       ),
